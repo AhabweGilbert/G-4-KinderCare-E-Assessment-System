ef5ed1738c8ee34c515da3ead021b6bfd525b0dd
AhabweGilbert <92158385+AhabweGilbert@users.noreply.github.com>
2022-01-31 17:57:45 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$password = '';
+$db = 'KinderCare';
+
+$connection = mysqli_connect($dbhost,$dbuser,$password,$db);
+
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+use function PHPSTORM_META\type;
+
+//session_start();//keeping track of the current logged in user
+session_start();
+include('connection.php');
+include('functions.php');
+
+$userdata = checkLogin($connection);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KinderCare E-Assessment System</title>
+    <link rel="stylesheet" href="css/newkc.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+   
+</head>
+
+<body>
+    <div class="main">
+    <?php
+    head_section();
+
+    section();
+    ?> <div class="body-text"> <?php
+    dashboard();
+    ?> </div>
+    
+</div>
+
+<script src="app.js"></script>
+<script src="try.js"></script>
+</body>
+<?php
+    footer_section();
+?>
+
+</html>
